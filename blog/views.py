cfb3507e8371c11c4f49ec7a2978122fee66f16b
Kehinde Ohiosumuan <ohiosumuankehinde@gmail.com>
2021-04-27 23:43:23 +0100

    first commit

---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+
+
+# Create your views here.
+def registerPage(request):
+
+    context = {}
+    return render(request, 'accounts/register')
+
+
+def loginPage(request):
+    
+    context = {}
+    return render(request, 'accounts/login')
+
+class BlogListView(ListView):
+
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
